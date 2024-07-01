370c0eb12e5bf390c768c3f0f0576905423d8e6f
michls-kmann <m.kaufmann959@gmail.com>
2024-07-01 17:42:34 +0200

    Update Kacheln.java

--- Miniprojekt1/src/basic/Kacheln.java
+++ Miniprojekt1/src/basic/Kacheln.java
@@ -1,3 +1,8 @@
+package basic;
+
+//import jserver.XSendAdapter;
+
+
 public class Kacheln {
 	public int x;
     public int y;
@@ -6,62 +11,64 @@ public class Kacheln {
     public String form = "s"; // Alle Kacheln als Quadrat
     public int wert;
     public static final int LEERE_FARBE = 0xcdc1b4;
+    public int [] farben = {0xeee4da, 0xede0c8, 0xf2b179, 0xf59563, 0xf67c5f, 0xf65e3b, 0xedcf72, 0xedcc61, 0xeac64e, 0xecc441, 0xeec22e };
+   
 
     public Kacheln(int x, int y, int wert) {
         this.x = x;
         this.y = y;
         this.wert = wert;
-        setFarben();
+        setFarben(farben);
     }
     
-    public void setFarben() {
+    public void setFarben(int farben[]) {
         switch (wert) {
             case 0:
                 this.farbe = LEERE_FARBE;
                 this.textFarbe = LEERE_FARBE; // Textfarbe gleich Hintergrundfarbe
                 break;
             case 2:
-                this.farbe = 0xeee4da;
+                this.farbe = farben[0];
                 this.textFarbe = 0x776e65;
                 break;
             case 4:
-                this.farbe = 0xede0c8;
+                this.farbe = farben[1];
                 this.textFarbe = 0x776e65;
                 break;
             case 8:
-                this.farbe = 0xf2b179;
+                this.farbe = farben[2];
                 this.textFarbe = 0xf9f6f2;
                 break;
             case 16:
-                this.farbe = 0xf59563;
+                this.farbe = farben[3];
                 this.textFarbe = 0xf9f6f2;
                 break;
             case 32:
-                this.farbe = 0xf67c5f;
+                this.farbe = farben[4];
                 this.textFarbe = 0xf9f6f2;
                 break;
             case 64:
-                this.farbe = 0xf65e3b;
+                this.farbe = farben[5];
                 this.textFarbe = 0xf9f6f2;
                 break;
             case 128:
-                this.farbe = 0xedcf72;
+                this.farbe = farben[6];
                 this.textFarbe = 0xf9f6f2;
                 break;
             case 256:
-                this.farbe = 0xedcc61;
+                this.farbe = farben[7];
                 this.textFarbe = 0xf9f6f2;
                 break;
             case 512:
-                this.farbe = 0xeac64e;
+                this.farbe = farben[8];
                 this.textFarbe = 0xf9f6f2;
                 break;
             case 1024:
-                this.farbe = 0xecc441;
+                this.farbe = farben[9];
                 this.textFarbe = 0xf9f6f2;
                 break;
             case 2048:
-                this.farbe = 0xeec22e;
+                this.farbe = farben[10];
                 this.textFarbe = 0xf9f6f2;
                 break;
             default:
